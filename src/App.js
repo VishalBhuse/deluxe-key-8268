2e6c6f6a2809ada639e5b418e265e35f6934dc6c
VishalBhuse <vbhuse1997@gmail.com>
2022-07-20 10:48:18 +0530

    shift

--- src/App.js
+++ src/App.js
@@ -1,11 +1,13 @@
 import './App.css';
 import { Footer } from './Component/Footer';
 import { Navbar } from './Component/Navbar';
+import { Navbarfirst } from './Component/Navbarfirst';
 
 function App() {
   return (
     <>
     <div>
+      <Navbarfirst />
     <Navbar />
     <Footer />
     </div>
---
+++ src/Component/Navbarfirst.jsx
@@ -0,0 +1,96 @@
+import React from "react";
+
+export const Navbarfirst = () => {
+  return (
+    <div>
+      <nav className="navbar navbar-expand-lg navbar-light " style={{backgroundColor:'#F7F8F9'}}>
+        <a className="navbar-brand" href="#">
+          <svg
+            width="48px"
+            height="48px"
+            viewBox="0 0 1024 1024"
+            data-aut-id="icon"
+            class=""
+            fill-rule="evenodd"
+          >
+            <path
+              class="rui-4K4Y7"
+              d="M661.333 256v512h-128v-512h128zM277.333 298.667c117.824 0 213.333 95.531 213.333 213.333s-95.509 213.333-213.333 213.333c-117.824 0-213.333-95.531-213.333-213.333s95.509-213.333 213.333-213.333zM794.496 384l37.504 37.504 37.504-37.504h90.496v90.496l-37.504 37.504 37.504 37.504v90.496h-90.496l-37.504-37.504-37.504 37.504h-90.496v-90.496l37.504-37.504-37.504-37.504v-90.496h90.496zM277.333 426.667c-47.061 0-85.333 38.293-85.333 85.333s38.272 85.333 85.333 85.333c47.061 0 85.333-38.293 85.333-85.333s-38.272-85.333-85.333-85.333z"
+            ></path>
+          </svg>
+        </a>
+        <button
+          className="navbar-toggler"
+          type="button"
+          data-toggle="collapse"
+          data-target="#navbarSupportedContent"
+          aria-controls="navbarSupportedContent"
+          aria-expanded="false"
+          aria-label="Toggle navigation"
+        >
+          <span className="navbar-toggler-icon"></span>
+        </button>
+
+        <div className="collapse navbar-collapse" id="navbarSupportedContent">
+          <ul className="navbar-nav mr-auto">
+            <li className="nav-item active">
+              <a className="nav-link" href="#">
+                <select class="custom-select custom-select-lg mb-3" style={{width:'250px'}}>
+                  <option selected>India</option>
+                  <option value="1">Maharshtra</option>
+                  <option value="2">Kerla</option>
+                  <option value="3">Goa</option>
+                  <option value="3">Kernatak</option>
+                </select>
+              </a>
+            </li>
+            <li className="nav-item">
+              <a className="nav-link" href="#">
+                Link
+              </a>
+            </li>
+            <li className="nav-item dropdown">
+              <a
+                className="nav-link dropdown-toggle"
+                href="#"
+                id="navbarDropdown"
+                role="button"
+                data-toggle="dropdown"
+                aria-haspopup="true"
+                aria-expanded="false"
+              >
+                Dropdown
+              </a>
+              <div className="dropdown-menu" aria-labelledby="navbarDropdown">
+                <a className="dropdown-item" href="#">
+                  Action
+                </a>
+                <a className="dropdown-item" href="#">
+                  Another action
+                </a>
+                <div className="dropdown-divider"></div>
+                <a className="dropdown-item" href="#">
+                  Something else here
+                </a>
+              </div>
+            </li>
+          </ul>
+          <form className="form-inline my-2 my-lg-0">
+            <input
+              className="form-control mr-sm-2"
+              type="search"
+              placeholder="Search"
+              aria-label="Search"
+            />
+            <button
+              className="btn btn-outline-success my-2 my-sm-0"
+              type="submit"
+            >
+              Search
+            </button>
+          </form>
+        </div>
+      </nav>
+    </div>
+  );
+};
